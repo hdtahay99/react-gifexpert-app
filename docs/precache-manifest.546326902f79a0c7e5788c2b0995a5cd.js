3920ea98346a5a6eb9302825006117ccf85a1920
Heansell Tahay <hdtahay@gmail.com>
2020-08-28 19:51:58 -0600

    docs added

---
+++ docs/precache-manifest.546326902f79a0c7e5788c2b0995a5cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8e02910ea76335f2a6f9a0d5f71055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7929efce2622b356f5",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7929efce2622b356f5",
+    "url": "/static/js/main.a1f2f1f9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
